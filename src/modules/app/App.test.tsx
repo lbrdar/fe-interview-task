434c3ab7dca2460665a9f205f76678bdebf5e629
lbrdar <Rosetta0107>
2021-09-02 00:47:10 +0200

    Fix tests

--- src/modules/app/App.test.tsx
+++ src/modules/app/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import { App } from './App';
+import { ApolloProvider } from '@apollo/client';
+import { apolloClient } from './apolloClient';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('should render successfully', () => {
+    expect(() => render(
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    )).not.toThrow();
+  });
 });
