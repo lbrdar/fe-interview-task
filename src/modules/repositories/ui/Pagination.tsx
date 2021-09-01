8c3231ade07a08baaffa0b0ca24b9109ab1f9f5d
lbrdar <Rosetta0107>
2021-09-02 00:29:41 +0200

    Add styling

--- src/modules/repositories/ui/Pagination.tsx
+++ src/modules/repositories/ui/Pagination.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
 import {
   IPageInfo,
-  ISearchRepositoriesData,
   ISearchRepositoriesVars
 } from '../queries/searchRepositories';
-import { ObservableQueryFields } from '@apollo/client/react/types/types';
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  margin-top: 20px;
+`;
+
+const StyledButton = styled.button`
+  font-size: 1em;
+  padding: 8px 16px;
+  outline: none;
+  background-color: lightblue;
+  cursor: pointer;
+  border: none;
+  box-shadow: 2px 2px 10px 2px rgba(0,0,0,0.3);
+`;
 
 interface IPaginationProps {
   pageInfo: IPageInfo;
-  fetchMore: ObservableQueryFields<ISearchRepositoriesData, ISearchRepositoriesVars>['fetchMore'];
+  fetchMore: (opts: { variables: ISearchRepositoriesVars }) => void;
   variables: ISearchRepositoriesVars;
 }
 
 export function Pagination({ pageInfo, variables, fetchMore }: IPaginationProps): React.ReactElement {
   return (
-      <div>
+      <StyledContainer>
         {pageInfo.hasNextPage && (
-          <button onClick={() => fetchMore({
+          <StyledButton onClick={() => fetchMore({
             variables: {
               ...variables,
               after: pageInfo.endCursor
             }
-          })}>Load more</button>
+          })}>Load more</StyledButton>
         )}
-      </div>
+      </StyledContainer>
   );
 }
--- src/modules/repositories/ui/RepositoriesListItem.tsx
+++ src/modules/repositories/ui/RepositoriesListItem.tsx
@@ -1,5 +1,22 @@
 import React from 'react';
 import { IRepository } from '../queries/searchRepositories';
+import styled from 'styled-components';
+
+const StyledListItem = styled.li`
+  list-style-type: none;
+  line-height: 2em;
+`;
+
+const StyledLink = styled.a`
+  font-size: 1.25em;
+  color: black;
+  text-decoration: none;
+  
+  &:hover {
+    cursor: pointer;
+    text-decoration: underline;
+  }
+`;
 
 interface IRepositoriesListItemProps {
   repository: IRepository;
@@ -9,10 +26,10 @@ function RepositoriesListItemBase({ repository }: IRepositoriesListItemProps): R
   const { url, nameWithOwner, stargazerCount, forkCount } = repository;
 
   return (
-    <li>
-      <a href={url} target="_blank" rel="noreferrer">{nameWithOwner} </a>
+    <StyledListItem>
+      <StyledLink href={url} target="_blank" rel="noreferrer">{nameWithOwner} </StyledLink>
       - 🌟 {stargazerCount} - 🍴 {forkCount}
-    </li>
+    </StyledListItem>
   );
 }
 
